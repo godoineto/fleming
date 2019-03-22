50488595d7d5b321c491465b3e51da01223765cd
Godoi <netogodoi.dev@gmail.com>
2019-03-22 17:00:38 -0300

    Trocando projeto para maven

---
+++ src/main/java/com/codenation/squad5/flemingapi/FlemingApiApplication.java
@@ -0,0 +1,13 @@
+package com.codenation.squad5.flemingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlemingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlemingApiApplication.class, args);
+	}
+
+}
