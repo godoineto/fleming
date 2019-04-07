c1502f6e883a872b018f47af111b227a3e59b2bf
Godoi <netogodoi.dev@gmail.com>
2019-04-07 14:51:56 -0300

    Ajustes no estoque e add junit

--- src/main/java/com/codenation/squad5/flemingapi/api/estoques/repository/EstoqueRepository.java
+++
@@ -1,7 +0,0 @@
-package com.codenation.squad5.flemingapi.api.estoques.repository;
-
-import com.codenation.squad5.flemingapi.api.estoques.model.Estoque;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface EstoqueRepository extends MongoRepository<Estoque, String> {
-}
