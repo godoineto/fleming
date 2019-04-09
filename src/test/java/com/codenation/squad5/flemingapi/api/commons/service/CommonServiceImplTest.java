db36e924fe174d0a06dcef08fc5d67ecc828e396
Natanael Emilio da Costa <natanaelemilio@gmail.com>
2019-04-09 20:25:13 -0300

    Teste DTO, naoNulo, classToDto adicionados às classes de teste

--- src/test/java/com/codenation/squad5/flemingapi/api/commons/service/CommonServiceImplTest.java
+++ src/test/java/com/codenation/squad5/flemingapi/api/commons/service/CommonServiceImplTest.java
@@ -2,23 +2,47 @@ package com.codenation.squad5.flemingapi.api.commons.service;
 
 import com.codenation.squad5.flemingapi.api.common.dto.EnderecoDTO;
 import com.codenation.squad5.flemingapi.api.common.model.Endereco;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CommonServiceImplTest {
-    private Endereco endereco = new Endereco();
-    /*private EnderecoDTO enderecoDTO;
-    private ModelMapper modelMapper = new ModelMapper();*/
+/*
+    @Autowired
+    private EnderecoController enderecoController;
 
-    @Before
-    public void setUp(){
+    private EnderecoDTO enderecoDTO = new EnderecoDTO();
+    private ModelMapper modelMapper = new ModelMapper();
+
+    @Test
+    public void shouldMapEnderecoToDto(){
+        Endereco endereco = this.setUp();
+        enderecoDTO = modelMapper.map(endereco, EnderecoDTO.class);
+        assertEquals( enderecoDTO, endereco);
+    }
+
+    @Test
+    public void naoNulo(){
+        assertNotNull(enderecoDTO);
+    }
+
+    @Test
+    public void deveSalvar(){
+        enderecoController.salvarEndereco(enderecoDTO);
+        assertFalse(enderecoController.listarEnderecos().contains(enderecoDTO));
+    }
+
+    private Endereco setUp(){
+        Endereco endereco = new Endereco();
         endereco.setBairro("Centro");
         endereco.setCep("35.960-000");
         endereco.setCidade("Santa Barbara");
@@ -28,14 +52,7 @@ public class CommonServiceImplTest {
         endereco.setRua("Pio XII");
         endereco.setLatitude("-19.96366832");
         endereco.setLongitude("-43.409729");
-    }
-
-    @Test
-    public void shouldMapEnderecoToDto(){
-        /*enderecoDTO = modelMapper.map(endereco, EnderecoDTO.class);
-        System.out.println(enderecoDTO.getBairro());*/
-    }
-
-
+        return endereco;
+    }*/
 
 }
